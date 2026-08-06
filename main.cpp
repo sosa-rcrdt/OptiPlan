a617b8bfd94e85040168ac3f0cbb4d721271c0e7
Nelson <sosafco3@gmail.com>
2026-08-05 19:04:54 -0600

    Feat: Add Initial OptiPlan Executable

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    const std::string appName = "OptiPlan";
+    const std::string appVersion = "0.0.1";
+
+    std::cout << appName << " v" << appVersion << "\n";
+    std::cout << "Planificador inteligente de tareas y horarios.\n";
+
+    return 0;
+}
